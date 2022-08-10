c0020967200fa85b9499f2d3fafba9e2986a69fb
MohitZNegi <79953686+MohitZNegi@users.noreply.github.com>
2022-08-10 22:09:18 +1200

    third commit

--- website/models.py
+++ website/models.py
@@ -2,7 +2,14 @@ from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
 
-
+class Meeting(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(10))
+    date = db.Column(db.Date, default=func.Date.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    mins = db.relationship('Minute')
+    def __repr__(self):
+        return '<Meet %r>' % self.id
 class Minute(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     dates = db.Column(db.String(10))
@@ -13,10 +20,13 @@ class Minute(db.Model):
     to_be_actionedby = db.Column(db.String(10))
     info = db.Column(db.String(10))
     date_of_comp = db.Column(db.String(10))
-
+    
    
     date = db.Column(db.DateTime(timezone=True), default=func.now())
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    meetings_id = db.Column(db.Integer, db.ForeignKey('meeting.id'))
+    def __repr__(self):
+        return '<Min %r>' % self.id
 
 
 class User(db.Model, UserMixin):
@@ -25,3 +35,6 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
     minutes = db.relationship('Minute')
+    meetings = db.relationship('Meeting')
+   
+    
--- website/views.py
+++ website/views.py
@@ -1,6 +1,9 @@
-from flask import Blueprint, render_template, request, flash, jsonify
+from tkinter import Button
+from tkinter.tix import Form
+from flask import Blueprint, redirect, render_template, request, flash, jsonify
 from flask_login import login_required, current_user
-from .models import Minute
+from numpy import delete
+from .models import Meeting, Minute, User
 from . import db
 import json
 
@@ -9,28 +12,60 @@ views = Blueprint('views', __name__)
 @views.route('/home')
 def Main():
     return render_template("home.html", user=current_user)
-
-    
 @views.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
-    if request.method == 'POST':
-        dates = request.form.get('minute.dates')
-        topic = request.form.get('minute.topic')
-        attendees = request.form.get('minute.attendees')
-        raised_by = request.form.get('minute.raised_by')
-        action = request.form.get('minute.action')
-        actionedby = request.form.get('minute.to_be_actionedby')
-        info = request.form.get('minute.info')
-        date_of_comp = request.form.get('minute.date_of_comp')
-        new_minute = Minute(dates=dates,topic=topic,attendees=attendees,raised_by=raised_by,
-        action=action,to_be_actionedby=actionedby,
-        info=info, date_of_comp=date_of_comp, user_id=current_user.id)
-        db.session.add(new_minute)
+
+   
+    if request.method == 'POST' :
+        
+        name = request.form.get('meeting.name')
+        date = request.form.get('meeting.date')
+        new_meeting = Meeting(name=name, date=date, user_id=current_user.id)
+        db.session.add(new_meeting)
         db.session.commit()
         flash('Note added!', category='success')
+        return redirect('/')
+    else:    
+        meets = Meeting.query.order_by(Meeting.id).all()
+    return render_template("minute_taker.html", user=current_user, meets=meets)
+    
+@views.route('/min/<int:id>', methods=['GET', 'POST'])
+@login_required
+def minute(id):
+    task = Meeting.query.get_or_404(id)
+    meetings_Id = request.get_data('meeting.id') #need to get table for each meeting sorted
+    user_id = current_user.id
+    id = current_user.id
+    if id == user_id: 
+        if request.method == 'POST':
+            dates = request.form.get('minute.dates')
+            topic = request.form.get('minute.topic')
+            attendees = request.form.get('minute.attendees')
+            raised_by = request.form.get('minute.raised_by')
+            action = request.form.get('minute.action')
+            actionedby = request.form.get('minute.to_be_actionedby')
+            info = request.form.get('minute.info')
+            date_of_comp = request.form.get('minute.date_of_comp')
+            new_minute = Minute(dates=dates,topic=topic,attendees=attendees,raised_by=raised_by,
+            action=action,to_be_actionedby=actionedby,
+            info=info, date_of_comp=date_of_comp, user_id=current_user.id)
+            db.session.add(new_minute, task)
+            db.session.commit()
+            flash('Note added!', category='success')
+    return render_template("min.html", user=current_user, task=task )
 
-    return render_template("minute_taker.html", user=current_user)
+@views.route('/delete/<int:id>')
+@login_required
+def delete(id):
+    
+    task = Meeting.query.get_or_404(id)
+    user_id = current_user.id
+    id = current_user.id
+    if id == user_id:
+        db.session.delete(task)
+        db.session.commit()
+        return redirect('/')
 
 
 @views.route('/delete-note', methods=['POST'])
